9f20c9eb414731cfb5dd67aae7317e3c84fd7514
Dominik Haitz <dominik.haitz@gmx.de>
2020-03-29 22:46:25 +0200

    add tests for convenience function

--- tests/tests.py
+++ tests/tests.py
@@ -13,9 +13,20 @@ def test_plotting_working():
     df = pd.DataFrame(values)
 
     df.plot(marker='o')
-    mplcyberpunk.make_lines_glow()
-    mplcyberpunk.add_underglow()
+    mplcyberpunk.add_glow_effects()
 
     plt.savefig("test.png")
 
+
+def test_plotting_working_individual_functions():
+    plt.style.use("cyberpunk")
+
+    values = {c: [random.randint(0, 10) for _ in range(7)] for c in 'ABCDEF'}
+    df = pd.DataFrame(values)
+
+    df.plot(marker='o')
+    mplcyberpunk.make_lines_glow()
+    mplcyberpunk.add_underglow()
+
+    plt.savefig("test_individual.png")
 
