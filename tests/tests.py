3801efc0661603404f0e8a859e86048261faabc7
Dominik Haitz <dominik.haitz@gmx.de>
2020-03-29 19:22:10 +0200

    add tests

---
+++ tests/tests.py
@@ -0,0 +1,21 @@
+# Sample Test passing with and pytest
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import mplcyberpunk
+import random
+
+
+def test_plotting_working():
+    plt.style.use("cyberpunk")
+
+    values = {c: [random.randint(0, 10) for _ in range(7)] for c in 'ABCDEF'}
+    df = pd.DataFrame(values)
+
+    df.plot(marker='o')
+    mplcyberpunk.make_lines_glow()
+    mplcyberpunk.add_underglow()
+
+    plt.savefig("test.png")
+
+
