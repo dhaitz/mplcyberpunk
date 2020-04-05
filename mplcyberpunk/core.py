90f569cf814ff71f4357e728674f349ec3ae42d5
Dominik Haitz <dominik.haitz@gmx.de>
2020-04-05 18:06:35 +0200

    rename vars, add comments

--- mplcyberpunk/core.py
+++ mplcyberpunk/core.py
@@ -13,7 +13,6 @@ def make_lines_glow(ax=None, n_glow_lines=10, diff_linewidth=1.05, alpha_line=0.
     """Add a glow effect to the lines in an axis object.
 
     Each existing line is redrawn several times with increasing width and low alpha to create the glow effect.
-    The line properties are copied as seen in this solution: https://stackoverflow.com/a/54688412/3240855
     """
     if not ax:
         ax = plt.gca()
@@ -28,19 +27,16 @@ def make_lines_glow(ax=None, n_glow_lines=10, diff_linewidth=1.05, alpha_line=0.
         linewidth = line.get_linewidth()
 
         for n in range(1, n_glow_lines + 1):
-            line_copy, = ax.plot(*data)
-            line_copy.update_from(line)
+            glow_line, = ax.plot(*data)
+            glow_line.update_from(line)  # line properties are copied as seen in this solution: https://stackoverflow.com/a/54688412/3240855
 
-            line_copy.set_alpha(alpha_value)
-            line_copy.set_linewidth(linewidth + (diff_linewidth * n))
-            line_copy.glow_line = True  # mark the glow lines, to disregard them in the underglow function.
+            glow_line.set_alpha(alpha_value)
+            glow_line.set_linewidth(linewidth + (diff_linewidth * n))
+            glow_line.is_glow_line = True  # mark the glow lines, to disregard them in the underglow function.
 
 
 def add_underglow(ax=None, alpha_underglow=0.1):
-    """Add an 'underglow' effect, i.e. faintly color the area below the line.
-
-    The line properties are copied as seen in this solution: https://stackoverflow.com/a/54688412/3240855
-    """
+    """Add an 'underglow' effect, i.e. faintly color the area below the line."""
     if not ax:
         ax = plt.gca()
 
@@ -48,7 +44,8 @@ def add_underglow(ax=None, alpha_underglow=0.1):
 
     for line in lines:
 
-        if hasattr(line, 'glow_line') and line.glow_line:
+        # don't add underglow for glow effect lines:
+        if hasattr(line, 'is_glow_line') and line.is_glow_line:
             continue
 
         x, y = line.get_data()
