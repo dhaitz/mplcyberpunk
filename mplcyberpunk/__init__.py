994d8a65a752e933c0e5378d954828b15fbdeeb5
Dominik Haitz <dominik.haitz@gmx.de>
2020-04-05 18:54:09 +0200

    get package version via pkg_resources

--- mplcyberpunk/__init__.py
+++ mplcyberpunk/__init__.py
@@ -1,14 +1,13 @@
 """mplcyberpunk - A new Python package"""
 
-__version__ = '0.1.0'
-__author__ = 'Dominik Haitz <dominik.haitz@gmx.de>'
-__all__ = []
-
-
 import matplotlib as mpl
 import pkg_resources
 
 from .core import add_glow_effects, make_lines_glow, add_underglow
+
+__version__ = pkg_resources.require("mplcyberpunk")[0].version
+__author__ = 'Dominik Haitz <dominik.haitz@gmx.de>'
+__all__ = []
 
 # register the included stylesheet in the mpl style library
 data_path = pkg_resources.resource_filename('mplcyberpunk', 'data/')
