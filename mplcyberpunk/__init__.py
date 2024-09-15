1711cf6a88f3c1ade4ec6d22d6e0fca4de671888
Steve C <diceroll123@gmail.com>
2024-09-14 20:27:37 -0400

    remove pkg_resources

--- mplcyberpunk/__init__.py
+++ mplcyberpunk/__init__.py
@@ -1,15 +1,17 @@
 """mplcyberpunk - A new Python package"""
 
+import os
 import matplotlib as mpl
-import pkg_resources
+import importlib.metadata
 
 from .core import add_glow_effects, make_lines_glow, add_underglow, make_scatter_glow, add_gradient_fill, add_bar_gradient
 
-__version__ = pkg_resources.require("mplcyberpunk")[0].version
+__version__ = importlib.metadata.version("mplcyberpunk")
 __author__ = 'Dominik Haitz <dominik.haitz@gmx.de>'
 __all__ = []
 
 # register the included stylesheet in the mpl style library
-data_path = pkg_resources.resource_filename('mplcyberpunk', 'data/')
+
+data_path = os.path.join(os.path.dirname(__file__), 'data/')
 cyberpunk_stylesheets = mpl.style.core.read_style_directory(data_path)
 mpl.style.core.update_nested_dict(mpl.style.library, cyberpunk_stylesheets)
